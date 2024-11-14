917b74b637477513089a1b59ed1d279623932ad1
tatecarson-dsu <88904112+tatecarson-dsu@users.noreply.github.com>
2024-11-14 13:32:52 -0600

    update QR code link to use hash routing for mobile access

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => (
   <div className="landing-page">
     <h1>Welcome to Sound Design Student Music Exhibit</h1>
     <p>Scan the QR code below to explore the site on your mobile device:</p>
-    <QRCodeCanvas className="qr-code" value="https://tatecarson.github.io/dad-maac-f-24.js/exhibits" size={256} />
+    <QRCodeCanvas className="qr-code" value="https://tatecarson.github.io/dad-maac-f-24.js/#/exhibits" size={256} />
     <p>Music and sound design from DAD Digital Sound Design Students: Jason Kaiser, Martin Bailey, Reiter Bodicker, Abbie Pillen, Owen Elsen, and Karl Lexvold.</p>
     <p>Please use headphones for an optimal listening experience.</p>
   </div>
